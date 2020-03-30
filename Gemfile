a11f82c94e0209d0355b87bd5df201700d255b45
Leonhard Gahr <leonhard.gahr@gmail.com>
2020-03-30 17:11:54 +0200

    add ssh connection

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'net-ssh', '>= 5.2.0'
+
 # Specify your gem's dependencies in teamspeak3-ruby.gemspec
 gemspec
--- lib/ruby.rb
+++ lib/ruby.rb
@@ -1,7 +1,37 @@
-require "teamspeak3/ruby/version"
+# frozen_string_literal: true
+
+require 'teamspeak3/version'
 
 module Teamspeak3
   module Ruby
-    # Your code goes here...
+    require 'net/ssh'
+
+    host = 'gahr.dev'
+    user = 'serveradmin'
+    port = 10_022
+    password = ''
+
+    command = 'serverlist'
+    Net::SSH.start(host,
+                   user,
+                   password: password,
+                   port: port,
+                   non_interactive: true,
+                   keepalive: true) do |ssh|
+
+      ssh.open_channel do |channel|
+        channel.send_channel_request 'shell' do |ch, success|
+          if success
+            puts 'shell requested'
+            ch.send_data "#{command}\n"
+            ch.on_data do |c, data|
+              puts data
+            end
+            ch.send_data "quit\n"
+          end
+        end
+      end
+      ssh.loop
+    end
   end
 end
